59738ad271ffd21a97d465a3beece3acacc3f123
Harpalsinh Jadeja <jadejaharpal14@gmail.com>
2024-03-23 21:35:37 +0530

    feat: degen tokens balances frame added

---
+++ airstack/find-degen-tokens-balance.ts
@@ -0,0 +1,91 @@
+import { fetchQueryWithPagination, init } from "@airstack/node";
+import { DocumentNode, gql } from "@apollo/client";
+import { formatEther } from "viem";
+
+init(process.env.AIRSTACK_API_KEY || "");
+
+type EnumToValue<T extends Record<string, any>, U> = {
+    [K in keyof T]: U;
+  };
+
+enum DEGEN_TOKENS {
+    "Degen",
+    "Base God",
+    "Normie",
+    "doginme",
+    "Toshi"
+}
+
+type TokenResult = {
+    amount: string,
+    token: {
+        name: DEGEN_TOKENS,
+        logo: string[]
+    }
+}
+
+type TokenBalance = TokenResult[];
+
+type TokenBalances = {
+    TokenBalance: TokenBalance
+}
+
+const query = gql`
+query FindDegenTokenBalances {
+    TokenBalances(
+      input: {filter: {owner:{_eq:"0x22b2DD2CFEF2018D15543c484aceF6D9B5435863"}, tokenAddress: {_in: ["0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed","0x0d97F261b1e88845184f678e2d1e7a98D9FD38dE", "0xAC1Bd2486aAf3B5C0fc3Fd868558b082a531B2B4", "0x6921B130D297cc43754afba22e5EAc0FBf8Db75b", "0x7F12d13B34F5F4f0a9449c16Bcd42f0da47AF200"]}, tokenType: {_eq: ERC20}}, blockchain: base}
+    ) {
+      TokenBalance {
+        amount
+        token {
+          name
+          logo {
+            small
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default async function findDegenTokenBalances(identity: string) {
+
+  let balances: any = {
+    "Degen": '0',
+    "Base God": '0',
+    "Normie": '0',
+    "doginme": '0',
+    "Toshi": '0'
+}
+
+    try {
+        let response = await fetchQueryWithPagination(gqlToString(query), {
+            address: identity,
+        });
+
+        if (response) {
+            let { data } = response;
+
+            
+
+            let tokenBalances = data.TokenBalances as TokenBalances;
+
+            let tokenBalance = tokenBalances.TokenBalance;
+
+            for(let i = 0; i < tokenBalance.length; i++) {
+                let tokenDetail = tokenBalance[i];
+
+                let { amount, token } = tokenDetail;
+                
+                balances[token.name] = formatEther(BigInt(amount));
+            }
+
+        }
+    } catch (e) {
+        console.error(e);
+    }
+    return balances;
+}
+
+export const gqlToString = (gqlQuery: DocumentNode): string =>
+    gqlQuery.loc?.source.body || "";
--- app/frames/degen-balance/route.tsx
+++ app/frames/degen-balance/route.tsx
@@ -1,24 +1,53 @@
 import { Button } from "frames.js/next";
 import { frames } from "../frames";
+import findDegenTokenBalances from "@/airstack/find-degen-tokens-balance";
+
+function balanceUpto2Decimals(str: string) {
+    // Regular expression to match numbers with potentially more than two decimal places
+    const regex = /^(\d+(?:\.\d{0,2})?)\d*$/;
+
+    // Check if the string matches the regular expression
+    if (regex.test(str)) {
+        // Extract the matched part with up to two decimal places
+        const truncatedString = str.replace(regex, "$1");
+        return truncatedString;
+    } else {
+        // Handle invalid input
+        return "Invalid number";
+    }
+}
 
 const handleRequest = frames(async (ctx) => {
+    let data = await findDegenTokenBalances(
+        "0x22b2DD2CFEF2018D15543c484aceF6D9B5435863"
+    );
+
+    console.log(balanceUpto2Decimals(data["Base God"]));
+
     return {
         image: (
             <div tw="flex flex-col items-center justify-center">
+                <div tw="flex items-center justify-center">
+                    <h2>Your Balances</h2>
+                </div>
                 <div tw="flex items-center justify-center">
                     <div tw="flex items-center space-x-2">
                         <img
                             tw="w-[80px] h-[80px] rounded-full"
                             src="https://assets.airstack.xyz/image/logo/8453/YJHU8e8eFBKEjHeu1z/w9xfYa2hi8kawEAZ4u8ZLTZENUduCoFOvZWjP+Ru2IEkL/small.png"
                         />
-                        <h2 tw="pl-[20px]">42</h2>
+                        <h2 tw="pl-[20px]">
+                            {balanceUpto2Decimals(data["Degen"])}
+                        </h2>
                     </div>
                     <div tw="flex pl-[200px] items-center space-x-2">
                         <img
                             tw="w-[80px] h-[80px] rounded-full"
                             src="https://assets.airstack.xyz/image/logo/8453/Kwt1lrnu48hVr84kcFMH3U6IRPPX0ekAfxGPEWIEzUomCanfgi/y8yYBFMCjnlrJ/small.png"
                         />
-                        <h2 tw="pl-[20px]">42</h2>
+                        <h2 tw="pl-[20px]">
+                            {balanceUpto2Decimals(data["Base God"])}
+                        </h2>
                     </div>
                 </div>
                 <div tw="flex items-center justify-center">
@@ -27,14 +56,18 @@ const handleRequest = frames(async (ctx) => {
                             tw="w-[80px] h-[80px] rounded-full"
                             src="https://assets.airstack.xyz/image/logo/8453/Q1I4YqH38O2uOFFg0u68YOhMaaDDBiLFx7p5rNSO9L1PHi4VADVMl8OMt/uDNEUF/small.png"
                         />
-                        <h2 tw="pl-[20px]">42</h2>
+                        <h2 tw="pl-[20px]">
+                            {balanceUpto2Decimals(data["Normie"])}
+                        </h2>
                     </div>
                     <div tw="flex pl-[200px] items-center space-x-2">
                         <img
                             tw="w-[80px] h-[80px] rounded-full"
                             src="https://assets.airstack.xyz/image/logo/8453/4SB8Hx1kdOpRV6XWhno0M2KPp5uA9Ey77a//PsfDhBrI4AI+HXHM9FBr33iQ2eio/small.jpg"
                         />
-                        <h2 tw="pl-[20px]">42</h2>
+                        <h2 tw="pl-[20px]">
+                            {balanceUpto2Decimals(data["Toshi"])}
+                        </h2>
                     </div>
                 </div>
                 <div tw="flex items-center justify-center">
@@ -43,7 +76,9 @@ const handleRequest = frames(async (ctx) => {
                             tw="w-[80px] h-[80px] rounded-full"
                             src="https://assets.airstack.xyz/image/logo/8453/UFrxiWJtVYLmOG7Qc3XlihM66Za8OjuIJM0Izlqkg2hiBMGCI4zCI+1s1bQ2sRKk/small.png"
                         />
-                        <h2 tw="pl-[20px]">42</h2>
+                        <h2 tw="pl-[20px]">
+                            {balanceUpto2Decimals(data["doginme"])}
+                        </h2>
                     </div>
                 </div>
             </div>
