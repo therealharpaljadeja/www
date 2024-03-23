be25265c6e85393734d851f66ad185e2ff8bf4ef
Harpalsinh Jadeja <jadejaharpal14@gmail.com>
2024-03-24 03:32:11 +0530

    feat: standardize design and add frame 2 & 3

--- app/frames/degen-tipper/route.tsx
+++ app/frames/degen-tipper/route.tsx
@@ -1,36 +1,35 @@
 /* eslint-disable react/jsx-key */
 import { Button } from "frames.js/next";
-import { frames } from "./frames";
+import { frames } from "../frames";
 
 const handleRequest = frames(async (ctx) => {
     return {
         image: (
             <div tw="w-full h-full bg-black text-white flex flex-col items-start">
-                <div tw="flex  w-full">
-                    <div tw="ml-[80px] mt-[100px] border-4 border-white w-[50px]"></div>
+                <div tw="flex w-full">
+                    <div tw="ml-[80px] mt-[100px] border-4 border-[#AEAEAE] w-[50px]"></div>
                     <div tw="ml-[10px] mt-[100px] border-4 border-[#AEAEAE] w-[50px]"></div>
-                    <div tw="ml-[10px] mt-[100px] border-4 border-[#AEAEAE] w-[50px]"></div>
+                    <div tw="ml-[10px] mt-[100px] border-4 border-white w-[50px]"></div>
                     <div tw="ml-[10px] mt-[100px] border-4 border-[#AEAEAE] w-[50px]"></div>
                     <div tw="ml-[10px] mt-[100px] border-4 border-[#AEAEAE] w-[50px]"></div>
                 </div>
-                <div tw=" w-full h-full flex flex-col pl-[80px]">
-                    <h2>www</h2>
-                    <p tw="text-[#AEAEAE]">
-                        Warpcast Wallet Wrapped is your yearly recap of on-chain
-                        actions. Find out if you are X, Y, Z or A.
+                <div tw=" w-full pt-[50px] h-full flex pl-[80px]">
+                    <img
+                        tw="w-[300px] h-[300px]"
+                        src="https://wrpcd.net/cdn-cgi/image/fit=contain,f=auto,w=168/https%3A%2F%2Fi.imgur.com%2FRYLwxg1.jpg"
+                    />
+                    <p tw="text-[#AEAEAE] mr-[120px] ml-[40px]">
+                        This past year, you were tipped $DEGEN many time by many
+                        people. However, who tipped the most? Let's find out!
                     </p>
                 </div>
             </div>
         ),
         buttons: [
-            <Button
-                action="post"
-                target={{
-                    pathname: "/degen-tips",
-                }}
-            >
-                Wrap Me!
+            <Button action="post" target={{ pathname: "/degen-tips" }}>
+                ← Previous
             </Button>,
+
             // <Button
             //     action="post"
             //     target={{
