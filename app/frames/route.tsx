a14181a3337640df19744834d031fb0d9ba3764b
Harpalsinh Jadeja <jadejaharpal14@gmail.com>
2024-03-24 02:54:41 +0530

    feat: first frame placeholder

--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -5,8 +5,21 @@ import { frames } from "./frames";
 const handleRequest = frames(async (ctx) => {
     return {
         image: (
-            <div tw="flex items-center justify-center">
-                <h2>Warpcast & Wallet Wrapped!</h2>
+            <div tw="w-full h-full bg-black text-white flex flex-col items-start justify-center">
+                <div tw="flex">
+                    <div tw="ml-[80px] mt-[100px] border-4 border-white w-[50px]"></div>
+                    <div tw="ml-[10px] mt-[100px] border-4 border-[#AEAEAE] w-[50px]"></div>
+                    <div tw="ml-[10px] mt-[100px] border-4 border-[#AEAEAE] w-[50px]"></div>
+                    <div tw="ml-[10px] mt-[100px] border-4 border-[#AEAEAE] w-[50px]"></div>
+                    <div tw="ml-[10px] mt-[100px] border-4 border-[#AEAEAE] w-[50px]"></div>
+                </div>
+                <div tw="flex flex-col w-[95%] pl-[80px] h-[90%]">
+                    <h2>www</h2>
+                    <p tw="text-[#AEAEAE]">
+                        Warpcast Wallet Wrapped is your yearly recap of on-chain
+                        actions. Find out if you are X, Y, Z or A.
+                    </p>
+                </div>
             </div>
         ),
         buttons: [
