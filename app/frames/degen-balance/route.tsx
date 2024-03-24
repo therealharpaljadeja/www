35ac0b3e5aedc6b7cdc4ebcfdd8eb3fe5cb4b7ea
Harpalsinh Jadeja <jadejaharpal14@gmail.com>
2024-03-24 18:45:02 +0530

    fix: build errors

--- app/frames/degen-balance/route.tsx
+++ app/frames/degen-balance/route.tsx
@@ -15,11 +15,26 @@ const handleRequest = frames(async (ctx) => {
             image: (
                 <div tw="w-full h-full bg-black text-white flex flex-col items-start">
                     <div tw="flex w-full">
-                        <div tw="ml-[80px] mt-[100px] border-4 border-[#AEAEAE] w-[50px]"></div>
-                        <div tw="ml-[10px] mt-[100px] border-4 border-[#AEAEAE] w-[50px]"></div>
-                        <div tw="ml-[10px] mt-[100px] border-4 border-[#AEAEAE] w-[50px]"></div>
-                        <div tw="ml-[10px] mt-[100px] border-4 border-[#AEAEAE] w-[50px]"></div>
-                        <div tw="ml-[10px] mt-[100px] border-4 border-white w-[50px]"></div>
+                        <div
+                            key={1}
+                            tw="ml-[80px] mt-[100px] border-4 border-[#AEAEAE] w-[50px]"
+                        ></div>
+                        <div
+                            key={2}
+                            tw="ml-[10px] mt-[100px] border-4 border-[#AEAEAE] w-[50px]"
+                        ></div>
+                        <div
+                            key={3}
+                            tw="ml-[10px] mt-[100px] border-4 border-[#AEAEAE] w-[50px]"
+                        ></div>
+                        <div
+                            key={4}
+                            tw="ml-[10px] mt-[100px] border-4 border-[#AEAEAE] w-[50px]"
+                        ></div>
+                        <div
+                            key={5}
+                            tw="ml-[10px] mt-[100px] border-4 border-white w-[50px]"
+                        ></div>
                     </div>
                     <div tw=" w-full h-full flex flex-col pl-[80px]">
                         <h2 tw="text-[40px]">Meme coin balance:</h2>
